405d538a887de7ce593b76461e2428106db4a153
Stefan Ladstätter <ladstaetter@matic.at>
2010-10-27 00:09:11 +0200

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   
   # Return a title on a per-page basis
-  def title
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if @title.nil?
-      base_title
+  def title                                               # method definition
+    base_title = "Ruby on Rails Tutorial Sample App"      # variable assignment
+    if @title.nil?                                        # boolean test for nil
+      base_title                                          # implicit return
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} | #{@title}"                         # string interpolation
     end
   end
 end
